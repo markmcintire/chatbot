c4b375ecfba399463a0ee2dd38d39d3d45564bbb
Mark McIntire <markmcintire@Marks-MacBook-Air.local>
2023-05-09 22:01:16 +0800

    fix

--- chatbot/config.py
+++ chatbot/config.py
@@ -5,7 +5,7 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     SECRET_KEY = "this-is-a-super-secret-key"
-    OPENAI_KEY = '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'
+    OPENAI_KEY = 'secret key'
 
 
 config = {
