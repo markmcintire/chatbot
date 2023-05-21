7e29cee7a52de9487813eca2726c9fdc339098c3
Gio <91263142+lotuspar@users.noreply.github.com>
2023-05-21 17:15:53 +0800

    Basic working history search

--- chatbot/history.py
+++ chatbot/history.py
@@ -1,5 +1,8 @@
 ﻿from flask import Flask, Blueprint, render_template, request, jsonify
 from flask_login import login_required, current_user
+from sqlalchemy import func, literal
+
+from chatbot.database import MessageRecord
 
 history = Blueprint('history', __name__)
 
@@ -8,3 +11,26 @@ history = Blueprint('history', __name__)
 @login_required
 def historypage():
     return render_template('history.html')
+
+
+@history.route('/search', methods=["POST"])
+@login_required
+def search():
+    request_data = request.json
+    query_text = request_data["query"]  # Simple string query
+
+    # Let's find conversations (or "prompts") with the query
+    query_result = MessageRecord.query \
+        .filter(func.lower(MessageRecord.message).contains(func.lower(literal(query_text)))) \
+        .filter(MessageRecord.role == "user") \
+        .group_by(MessageRecord.chat_id)
+
+    response_dict = {"result": []}
+
+    for record in query_result:
+        response_dict["result"].append({
+            "message": record.message,
+            "chat_id": record.chat_id
+        })
+
+    return jsonify(response_dict)
---
+++ chatbot/static/js/history.js
@@ -0,0 +1,34 @@
+﻿const searchResultContainer = $("#containerSearchResult");
+const searchInputBox = $("#inputSearch");
+
+function requestSearch(query) {
+    // Wipe container results
+    searchResultContainer[0].innerHTML = '';
+
+    // Send a POST to /search with our query
+    fetch("/search", {
+        method: "POST", headers: {
+            'Content-Type': 'application/json'
+        }, body: JSON.stringify({query})
+    })
+        .then(response => response.json())
+        .then(json => populateSearchResults(json));
+}
+
+function populateSearchResults(json) {
+    json.result.forEach(v => {
+        const el = document.createElement("button");
+        el.type = "button";
+        el.classList.add("list-group-item");
+        el.classList.add("list-group-item-action");
+        el.innerText = v.message;
+        searchResultContainer[0].appendChild(el);
+    });
+}
+
+searchInputBox.on("input", function (e) {
+    const value = searchInputBox.val();
+    requestSearch(value);
+});
+
+requestSearch("");
