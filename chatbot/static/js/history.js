5a2d8cf1c8c8e7f3be9a8c943f52dba2aec39706
Gio <91263142+lotuspar@users.noreply.github.com>
2023-05-21 17:22:10 +0800

    Clean code, prepare for chat history impl

--- chatbot/static/js/history.js
+++ chatbot/static/js/history.js
@@ -1,6 +1,11 @@
 ﻿const searchResultContainer = $("#containerSearchResult");
 const searchInputBox = $("#inputSearch");
+const chatAreaContainer = $("#chatArea");
 
+/**
+ * Request conversations with a suitable message based on query
+ * @param query Message query
+ */
 function requestSearch(query) {
     // Wipe container results
     searchResultContainer[0].innerHTML = '';
@@ -15,6 +20,10 @@ function requestSearch(query) {
         .then(json => populateSearchResults(json));
 }
 
+/**
+ * Populate the search results using JSON from the server
+ * @param json JSON from /search
+ */
 function populateSearchResults(json) {
     json.result.forEach(v => {
         const el = document.createElement("button");
@@ -22,6 +31,10 @@ function populateSearchResults(json) {
         el.classList.add("list-group-item");
         el.classList.add("list-group-item-action");
         el.innerText = v.message;
+        el.record = v;
+        el.onclick = (e) => {
+
+        };
         searchResultContainer[0].appendChild(el);
     });
 }
@@ -30,5 +43,31 @@ searchInputBox.on("input", function (e) {
     const value = searchInputBox.val();
     requestSearch(value);
 });
+
+/**
+ * Request the chat history for a conversation
+ * @param chat_id Conversation / chat ID
+ */
+function requestChatHistory(chat_id) {
+    // Wipe current chat history
+    chatAreaContainer[0].innerHTML = '';
+
+    // Send a POST to /history with our query
+    fetch("/history", {
+        method: "POST", headers: {
+            'Content-Type': 'application/json'
+        }, body: JSON.stringify({chat_id})
+    })
+        .then(response => response.json())
+        .then(json => populateChatHistory(json));
+}
+
+/**
+ * Populate the chat history using JSON from the server
+ * @param json JSON from /history
+ */
+function populateChatHistory(json) {
+
+}
 
 requestSearch("");
